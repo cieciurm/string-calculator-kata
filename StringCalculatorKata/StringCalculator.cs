ceff1050deab46ab1b15c58a1320bdd89216433d
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2021-06-15 14:55:20 +0200

    Implement sum for 0,1 or 2 arguments

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Linq;
+
 namespace StringCalculatorKata
 {
     public class StringCalculator
     {
         public int Add(string numbers)
         {
-            return 0;
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var splitted = numbers.Split(',');
+
+            if (!splitted.Any())
+            {
+                return 0;
+            }
+
+            return splitted
+                .Select(x => Convert.ToInt32(x))
+                .Sum();
         }
     }
 }
--- StringCalculatorKata/StringCalculatorTests.cs
+++ StringCalculatorKata/StringCalculatorTests.cs
@@ -25,6 +25,19 @@ namespace StringCalculatorKata
             Check.That(result).IsZero();
         }
 
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void Add_WhenNumbersInclude0_1_2Integers_ThenSumsCorrectly(string input, int expected)
+        {
+             // Act
+            var result = Act(input);
+
+            // Assert
+            Check.That(result).IsEqualTo(expected);
+        }
+
         private int Act(string input) => _calculator.Add(input);
     }
 }
