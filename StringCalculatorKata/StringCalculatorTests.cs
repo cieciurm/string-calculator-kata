637495580b6e3fc513598d8d3fa0b38ae4a4a590
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2021-06-15 14:56:44 +0200

    Add method to handle an unknown number of numbers

--- StringCalculatorKata/StringCalculatorTests.cs
+++ StringCalculatorKata/StringCalculatorTests.cs
@@ -38,6 +38,18 @@ namespace StringCalculatorKata
             Check.That(result).IsEqualTo(expected);
         }
 
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("4,5,6", 15)]
+        public void Add_WhenNumbersIncludeUnknownNumberOfIntegers_ThenSumsCorrectly(string input, int expected)
+        {
+             // Act
+            var result = Act(input);
+
+            // Assert
+            Check.That(result).IsEqualTo(expected);
+        }
+
         private int Act(string input) => _calculator.Add(input);
     }
 }
